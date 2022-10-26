7ef01b0f0c8a13293d14d9d3082a112419fa4958
RichGuilherme <richard.guilhermeAS@hotmail.com>
2022-10-25 23:02:46 -0300

    versão 2.0

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-const enviarAlimento = document.getElementById("enviarProduto");
-const categorias = document.getElementById("enviarCategoria");
-const carrinho = document.getElementById("listaCompleta");
+const enviarAlimento = document.getElementById("enviarProduto");// aqui eu pego o input enviado pelo usuário ao clicar no botão
+const categorias = document.getElementById("enviarCategoria"); 
+const carrinho = document.getElementById("listaCompleta");    
 
-const produtos = document.getElementById("itens");
-const enviarProduto = (event) => {
-    if (produtos.value > "") {
+const produtos = document.getElementById("alimentos");
+const enviarProduto = (event) => {    //essa função serve pra pega o value do input="text" e salva em um variável o valor. Além de desabilitar ou habilitar
+    if (produtos.value > "") {         // a parte de categorias no html.
         alimento = produtos.value
-        produtos.value = ""
+        produtos.value = ""            //o elemento html do text, ficará vazio após o envio do alimento. 
         
         opcoesDeCategoria.removeAttribute("disabled");
         enviarCategoria.removeAttribute("disabled");   
@@ -14,11 +14,11 @@ const enviarProduto = (event) => {
         
         produtos.style.borderColor = "black"
     }else{
-        produtos.style.borderColor = "red"
+        produtos.style.borderColor = "red"     // caso o input seja feito vazio, a box do texto irá ficar vermelha como alerta.
     }
 }   
 
-var categoriaEAlimento = [
+var categoriaEAlimento = [          // array objeto com a categorias dos produtos é um array para eles armazenarem os alimentos enviado pelo usuário..
    {"categoria": "carne", 
     "produtos": []},
    {"categoria": "hortifruti",
@@ -36,13 +36,13 @@ var categoriaEAlimento = [
    {"categoria": "bebida", 
    "produtos": []}
 ]
-const enviarOpcao = (event) => {
-const select = document.getElementById("opcoesDeCategoria");    //completo para encontra o value do option
-const option = select.options[select.selectedIndex].value; // obter o value do option
+const enviarOpcao = (event) => {    
+const select = document.getElementById("opcoesDeCategoria");   
+const option = select.options[select.selectedIndex].value;     
 
-categoriaEAlimento.forEach(categoriaDoProduto => {         
-     if((categoriaDoProduto.produtos.length > 1)&&(categoriaDoProduto.categoria == option) ){
-        categoriaDoProduto.produtos.push(alimento)
+categoriaEAlimento.forEach(categoriaDoProduto => {        
+     if((categoriaDoProduto.produtos.length > 1)&&(categoriaDoProduto.categoria == option) ){ // Pecorre o array e verificar se a chave produto esta vazia e compara a chave categoria com a option
+        categoriaDoProduto.produtos.push(alimento)                                            // selecionada pelo usuário.
 
         event.preventDefault()
         opcoesDeCategoria.setAttribute("disabled", "");
@@ -64,7 +64,7 @@ const fazerLista = () => {
 const lista = document.createElement("ul")
 
 categoriaEAlimento.forEach(atributos => {  
-    if(atributos.produtos.length > 0){ 
+    if(atributos.produtos.length > 0){         // só criará o elemento na tela se o produto estever com algum valor.
      const listaItens = document.createElement('li')
      
      const texto = document.createTextNode(`  ${atributos.categoria} : ${atributos.produtos}`)
@@ -86,6 +86,6 @@ const enviarListaCompleta = (event) => {
 }
 
 
-carrinho.addEventListener("click", enviarListaCompleta);
-categorias.addEventListener("click", enviarOpcao);
-enviarAlimento.addEventListener("click", enviarProduto);
+carrinho.addEventListener("click", enviarListaCompleta);  
+categorias.addEventListener("click", enviarOpcao); 
+enviarAlimento.addEventListener("click", enviarProduto); 
